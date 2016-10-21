ee9b33887e07d6a05e7dd6eb67cde35c12590ed7
unknown <>
2016-10-21 17:50:06 +0800

    page

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,5 +1,5 @@
 <template>
-	<ul>
+	<ul class="showul">
 		<li v-for="j in showmsg" @click="tolink(j.link)">
 	<div class="hello">
 		
@@ -11,29 +11,9 @@
 					 <span class="spking">{{j.category|zqwqw}}</span>
    			</div>
    			<div class="article">
-			
-				   {{j.description|zqw}}
-			
-			 <!--这篇文章主要介绍了JavaScript将XML转成JSON的方法,实例分析了javascript操作XML文件及格式转化的技巧,具有一定参考借鉴价值,需要的朋友可以参考下	 	 这篇文章主要介绍了JavaScript将XML转成JSON的方法,实例分析了javascript操作XML文件及格式转化的技巧,具有一定参考借鉴价值,需要的朋友可以参考下
-			 这篇文章主要介绍了JavaScript将XML转成JSON的方法,实例分析了javascript操作XML文件及格式转化的技巧,具有一定参考借鉴价值,需要的朋友可以参考下
-			 这篇文章主要介绍了JavaScript将XML转成JSON的方法,实例分析了javascript操作XML文件及格式转化的技巧,具有一定参考借鉴价值,需要的朋友可以参考下
-			 这篇文章主要介绍了JavaScript将XML转成JSON的方法,实例分析了javascript操作XML文件及格式转化的技巧,具有一定参考借鉴价值,需要的朋友可以参考下
-			 这篇文章主要介绍了JavaScript将XML转成JSON的方法,实例分析了javascript操作XML文件及格式转化的技巧,具有一定参考借鉴价值,需要的朋友可以参考下
- 这篇文章主要介绍了JavaScript将XML转成JSON的方法,实例分析了javascript操作XML文件及格式转化的技巧,具有一定参考借鉴价值,需要的朋友可以参考下
-			 这篇文章主要介绍了JavaScript将XML转成JSON的方法,实例分析了javascript操作XML文件及格式转化的技巧,具有一定参考借鉴价值,需要的朋友可以参考下
-			 这篇文章主要介绍了JavaScript将XML转成JSON的方法,实例分析了javascript操作XML文件及格式转化的技巧,具有一定参考借鉴价值,需要的朋友可以参考下
-
-
-
-			 	 	 这篇文章主要介绍了JavaScript将XML转成JSON的方法,实例分析了javascript操作XML文件及格式转化的技巧,具有一定参考借鉴价值,需要的朋友可以参考下
-			 这篇文章主要介绍了JavaScript将XML转成JSON的方法,实例分析了javascript操作XML文件及格式转化的技巧,具有一定参考借鉴价值,需要的朋友可以参考下
-			 这篇文章主要介绍了JavaScript将XML转成JSON的方法,实例分析了javascript操作XML文件及格式转化的技巧,具有一定参考借鉴价值,需要的朋友可以参考下
-			 这篇文章主要介绍了JavaScript将XML转成JSON的方法,实例分析了javascript操作XML文件及格式转化的技巧,具有一定参考借鉴价值,需要的朋友可以参考下
-			 这篇文章主要介绍了JavaScript将XML转成JSON的方法,实例分析了javascript操作XML文件及格式转化的技巧,具有一定参考借鉴价值,需要的朋友可以参考下
- 这篇文章主要介绍了JavaScript将XML转成JSON的方法,实例分析了javascript操作XML文件及格式转化的技巧,具有一定参考借鉴价值,需要的朋友可以参考下
-			 这篇文章主要介绍了JavaScript将XML转成JSON的方法,实例分析了javascript操作XML文件及格式转化的技巧,具有一定参考借鉴价值,需要的朋友可以参考下
-			 这篇文章主要介绍了JavaScript将XML转成JSON的方法,实例分析了javascript操作XML文件及格式转化的技巧,具有一定参考借鉴价值,需要的朋友可以参考下-->
-   			</div>
+				   有限公司于2015年9月成立。皮肤宝是一款垂直于皮肤科领域的移动医疗工具，医生端和患者端均于2016年3月20日正式上线运营，通过“线上首诊、线下转诊、急慢分治、上下联动、电子处方、药品到家”模式，实现皮肤病的在线分级诊疗。</p><p>王峰告诉36氪一组数据：<strong>皮肤宝APP上入驻医生已超过2000名，以公立三甲医院执业医生为主。与皮肤宝平台签约的合作医院超过300家。C端注册用户则已逾10万名，平台已经为近5万用户提供不同的医疗服务，其中付费用户有3万余名，平均客单价在百元左右（问诊和药品是主要赢利点）。复诊和复购用户合计超过10%，用户满意度超过85%。</strong></p><p><strong>除了继续投入核心问诊技术与产品的开发，皮肤宝近期还在大力开展B端业务。</strong>“我们希望，以皮肤宝核心的皮肤诊疗服务为切入点，向周边行业发散延展，打造皮肤健康生态大平台，一站式实现更美更健康的目标。”</p><p>王峰还透露，这些B端机构大体分为两类：<strong>一是综合医疗大健康，二是与美相关。</strong>目前，皮肤宝已与相关企业达成合作，并正在做技术对接。</p><p>虽然王峰表示B端合作方具体名字不方便透露，但通过投资方的观察和思考或许能略猜一二：</p><p>公开资料显示，隆领投资是由蔡文胜创办的早期股权投资机构，而蔡文胜也是美图秀秀的董事长。一个是美图/美拍，通过数字技术美化用户的图片或者视频效果，达到“表面”上的更好；一个是美肤，通过医疗技术或服务让用户的皮肤更健康，达到“本质”上的更好。那些不满足“表面”，且对“本质”有刚需的的用户，显然不在少数。再深层次的，美图投过的美妆企业qqq
+				 <!--{{j.description|zqw}}-->
+			</div>
    		</div>
    		<div class="flex_1">
    			<div class="showtime">
@@ -79,14 +59,16 @@ export default {
 					
 						vm.msg=[...msg]
 						vm.setmsg(1)
-						console.log(JSON.stringify(msg[0]))
+						//console.log(JSON.stringify(msg[0]))
 	              })
 		},
 		setmsg:function(n){
 			this.showloding=false
+			console.log(n)
 			if(n*10>this.msg.length) return
 			var s=this.msg.splice(0,n*10)
-			this.showmsg=s	
+			this.showmsg=s
+			this.page++
 		},
 		tolink:function(n){
 		//	alert(n)
@@ -104,8 +86,9 @@ export default {
 				
 				if(stop>=sheight-bheight){//当滚动条到顶部的距离等于滚动条高度减去窗口高度时
 					vm.showloding=true
-					vm.page++
+					
 					setTimeout(function(){
+						
 						vm.setmsg(vm.page)
 					},500)
 					
@@ -120,7 +103,18 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped >
-
+.showul{
+	position:absolute;
+	left:0;
+	right:0;
+	top:0;
+	bottom:0;
+	margin-right:0.2rem;
+}
+.showul li{
+	width:100%;
+	
+}
 .hello{
 	margin: 0 0.1rem;
 	
@@ -140,7 +134,7 @@ export default {
 }
 .flex_2{
 	flex:2;
-	margin-right:0.2rem;
+	
 	
 }
 .author{
